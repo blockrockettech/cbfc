c08903983e9b3aed1fbf037aff60d3902174b010
Andy Gray <andy@whatsthescore.co.uk>
2018-09-07 09:36:49 +0100

    mocking up cards

--- src/components/pages/MyCard.vue
+++ src/components/pages/MyCard.vue
@@ -5,19 +5,27 @@
         <img class="card-img-top" :src="`${lookupBoxCardSet(tokenId)[8].image}`" :alt="web3.utils.toAscii(lookupBoxCardSet(tokenId)[4])"/>
         <div class="card-body">
           <span class="badge badge-primary float-right">{{ tokenId.toString(10) }}</span>
-          <h5 class="card-title m-3">
+          <h5 class="card-title">
             {{ web3.utils.toAscii(lookupBoxCardSet(tokenId)[4]) }}
           </h5>
         </div>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">
-            Box <span class="">#{{ simpleBoxNumberFromTokenId(tokenId) }}</span>
+            <!--<span class="text-muted">Box</span> <span class="">{{ simpleBoxNumberFromTokenId(tokenId) }}</span> &bull;-->
+            <!--<span class="text-muted">Card</span> <span class="">{{ simpleBoxCardSetNumberFromTokenId(tokenId) }}</span> &bull;-->
+            <span class="text-muted small">Serial</span> <span class="float-right">#{{ boxCardSetSerialNumberFromTokenId(tokenId) }} of 100</span>
           </li>
           <li class="list-group-item">
-            Card <span class="">#{{ simpleBoxCardSetNumberFromTokenId(tokenId) }}</span>
+            <div class="row">
+              <div class="col"><span class="text-muted small">Power</span> <span class="float-right font-weight-bold">86</span></div>
+              <div class="col"><span class="text-muted small">Speed</span> <span class="float-right font-weight-bold">43</span></div>
+            </div>
           </li>
           <li class="list-group-item">
-            Serial <span class="">#{{ boxCardSetSerialNumberFromTokenId(tokenId) }}</span>
+            <div class="row">
+              <div class="col"><span class="text-muted small">Intelligence</span> <span class="float-right font-weight-bold">69</span></div>
+              <div class="col"><span class="text-muted small">Moral</span> <span class="float-right font-weight-bold">77</span></div>
+            </div>
           </li>
         </ul>
       </div>
